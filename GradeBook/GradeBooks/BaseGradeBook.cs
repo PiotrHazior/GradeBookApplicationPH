c85d654bf84b358da71bfb07d09388946324cb99
PiotrHazior <piotrhazior02@gmail.com>
2023-04-01 18:57:45 +0200

    Add Type property

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -263,8 +263,16 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
+        
+          public GradeBookType Type { get; set; }
+
+        
+
+
+
+
     }
 }
