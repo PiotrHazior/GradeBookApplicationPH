4e59a3bb8f05990544488d4bdbdddb35e7607e26
PiotrHazior <piotrhazior02@gmail.com>
2023-04-02 15:44:36 +0200

    StartingUserInterface's CreateCommand method

--- GradeBook/StandardGradeBook.cs
+++ GradeBook/StandardGradeBook.cs
@@ -7,8 +7,8 @@ using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
-    {
-        public StandardGradeBook(string name, bool isWeighted) : base(name) 
+    {     
+        public StandardGradeBook(string name) : base(name) 
         {
             Type = GradeBookType.Standard;
         }
